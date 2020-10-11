58a7d7163f92b87162eb4ae053007be79258bf14
Вася Пупкин <dimwin4@gmail.com>
2020-10-11 04:17:11 +0300

    Доработан бот и добавлена новая графика

--- Assets/Scripts/Enemies/Bug/BugAI.cs
+++ Assets/Scripts/Enemies/Bug/BugAI.cs
@@ -1,4 +1,8 @@
-﻿using Pathfinding;
+﻿using System;
+using System.Collections;
+using Pathfinding;
+using Pathfinding.Util;
+using TreeEditor;
 using UnityEngine;
 
 namespace Enemies.Bug
@@ -6,6 +10,8 @@ namespace Enemies.Bug
     public class BugAI : MonoBehaviour
     {
         public Rigidbody2D target;
+        public Transform targetForBug;
+        private Vector2 trgt;
         public float speed = 200f;
         public float nextWayPoyntDistance = 1f;
 
@@ -16,10 +22,33 @@ namespace Enemies.Bug
         private Seeker seeker;
         [SerializeField] private Rigidbody2D rb;
 
+        private bool _isFacingLeft = true;
+
+        [SerializeField] private float jumpForce = 50f; 
+        public bool isOnGround;
+        public bool canJump = true;
+        [SerializeField] private float rayCastLength;
+        public LayerMask groundLayer;
+
         void Start()
         {
             seeker = FindObjectOfType<Seeker>();
-            seeker.StartPath(rb.position, target.position, OnPathComplete);
+            
+            InvokeRepeating("UpdatePath", 0f, 0.5f);
+        }
+
+        void UpdatePath()
+        {
+            if (targetForBug != null && seeker.IsDone())
+            {
+                seeker.StartPath(rb.position, trgt, OnPathComplete);
+            }
+        }
+
+        void Update()
+        { 
+            isOnGround = Physics2D.Raycast(rb.position, Vector2.down, rayCastLength, groundLayer);
+            
         }
 
         void FixedUpdate()
@@ -37,15 +66,27 @@ namespace Enemies.Bug
 
             _isReachedEndOfPath = false;
 
-            Vector2 direction = ((Vector2) _path.vectorPath[_currentWaypoynt] - rb.position).normalized;
-            Vector2 force = direction * (speed * Time.deltaTime);
-            rb.AddForce(force);
+            float direction = _path.vectorPath[_currentWaypoynt].x - rb.position.x;
+            rb.AddForce(Vector2.right * (direction * speed * Time.deltaTime));
+
+            if (Mathf.Abs(rb.position.x - _path.vectorPath[_currentWaypoynt].x) < 2f &&
+                _path.vectorPath[_currentWaypoynt].y - rb. position.y > 1f && isOnGround && canJump)
+            {
+                rb.AddForce(Vector2.up * jumpForce,ForceMode2D.Impulse);
+                canJump = false;
+                StartCoroutine(JumpTimer());
+            }
 
             float distance = Vector2.Distance(rb.position, _path.vectorPath[_currentWaypoynt]);
 
             if (distance < nextWayPoyntDistance)
             {
                 _currentWaypoynt++;
+            }
+
+            if (rb.velocity.x < 0 && !_isFacingLeft || rb.velocity.x > 0 && _isFacingLeft)
+            {
+                Flip();
             }
         }
 
@@ -55,6 +96,41 @@ namespace Enemies.Bug
             {
                 _path = p;
                 _currentWaypoynt = 0;
+            }
+        }
+        
+        void Flip()
+        {
+            _isFacingLeft = !_isFacingLeft;
+            transform.rotation = Quaternion.Euler(0, _isFacingLeft ? 0 : 180, 0);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(rb.position, rb.position + Vector2.down * rayCastLength);
+        }
+
+        private IEnumerator JumpTimer()
+        {
+            yield return new WaitForSeconds(1f);
+            canJump = true;
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                targetForBug = other.gameObject.transform;
+                trgt = new Vector2(targetForBug.position.x, targetForBug.position.y);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                targetForBug = null;
             }
         }
     }
